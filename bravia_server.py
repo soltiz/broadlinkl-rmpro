6ab2c8865f58552e4fefbf30184fd2dcb3bee6fa
cedric <ceric@dmz>
2022-01-01 14:26:38 +0000

    bravia server working for wolume up/down

--- bravia_server.py
+++ bravia_server.py
@@ -1,7 +1,48 @@
+import broadlink
+import time
+import sys
+import fileinput
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/yellw")
-def hello_world():
-    print("hello world\n")
-    return "<p>Hello, World!</p>"
+app = Flask(__name__)
+device = broadlink.rm(("broadlink",80), bytearray.fromhex("780f77fd3d92"), 0x2712)
+device.auth()
+print ("Connecting to Broadlink device....")
+time.sleep(1)
+print ("Connected....")
+
+
+def sendIr(command):
+   with open(command + ".ir","br") as fin:
+      ir_packet=fin.read()
+   device.send_data(ir_packet)
+   print (" '%s' Code Sent...."%(command))
+
+@app.route("/chup")
+def increaseNadVolume():
+    sendIr('up')
+    return "<status>OK</status>"
+
+@app.route("/chdown")
+def decreaseNadVolume():
+    sendIr('down')
+    return "<status>OK</status>"
+
+@app.route("/yellow")
+def muteNad():
+    sendIr('mute')
+    return "<status>OK</status>"
+
+@app.route("/green")
+def switchOnNad():
+    sendIr('on')
+    return "<status>OK</status>"
+
+@app.route("/red")
+def switchOffNad():
+    sendIr('down')
+    sendIr('down')
+    sendIr('off')
+    return "<status>OK</status>"
+
+
---
+++ bravia_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export FLASK_APP=bravia_server.py
+flask run --port 80 --host 0.0.0.0
